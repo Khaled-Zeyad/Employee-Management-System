5571c7391f8e7c848b9c56316b8b2b041ff4e3b8
Rwx Hunter <arleyaoung1@gmail.com>
2026-01-30 15:16:00 +0200

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
@@ -6,6 +7,15 @@ app.get("/", (req, res) => {
   res.sendFile("./views/home.html", { root: __dirname });
 });
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
-});
+mongoose
+  .connect(
+    "mongodb+srv://rwx:iZF0bCNsUgAvlqie@cluster0.q1fas28.mongodb.net/all-data?retryWrites=true&w=majority",
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
